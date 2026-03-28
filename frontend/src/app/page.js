30ae796c0dab838dbed43ffc2294defe99aa0dde
Sid Mittal <sidmittal19@gmail.com>
2026-03-28 13:59:58 -0700

    Create basic page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,65 +1,43 @@
-import Image from "next/image";
+// src/app/page.js
+// extremely basic and barebones app to test if routes work
+"use client"
+
+import { useState } from "react"
+import { getImage, predict } from "@/lib/api"
 
 export default function Home() {
+  const [imgId, setImgId] = useState(null)
+  const [image, setImage] = useState(null)
+  const [predictedImage, setPredictedImage] = useState(null)
+
+  async function handleGetImage() {
+    const data = await getImage()
+    setImgId(data.img_id)
+    setImage(data.image)
+    setPredictedImage(null)  // reset prediction on new image
+  }
+
+  async function handlePredict() {
+    if (!imgId){ console.log("no image id"); return}  
+    const data = await predict(imgId)
+    console.log("I have the data")
+    setPredictedImage(data.image)
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <main>
+      <button onClick={handleGetImage}>Get Image</button>
+
+      {image && (
+        <>
+          <img src={`data:image/jpeg;base64,${image}`} alt="xray" /> {/* JSX can directly decode base64 bytes! */ }
+          <button onClick={handlePredict}>Run Prediction</button>
+        </>
+      )}
+
+      {predictedImage && (
+        <img src={`data:image/jpeg;base64,${predictedImage}`} alt="prediction" />
+      )}
+    </main>
+  )
 }
